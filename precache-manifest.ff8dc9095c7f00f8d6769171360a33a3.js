acd932ebb32d111c80e201c5a34fb4a16f120862
Bhargab Nath <73776527+amare-eng@users.noreply.github.com>
2022-01-02 21:08:13 -0800

    Updates

---
+++ precache-manifest.ff8dc9095c7f00f8d6769171360a33a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908fdf16c61a520575e60ed3b61cad62",
+    "url": "/cahoots/index.html"
+  },
+  {
+    "revision": "e386e85f49b220254c05",
+    "url": "/cahoots/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "1b7799288b73964b6e76",
+    "url": "/cahoots/static/css/main.c2baeed5.chunk.css"
+  },
+  {
+    "revision": "e386e85f49b220254c05",
+    "url": "/cahoots/static/js/2.80f76055.chunk.js"
+  },
+  {
+    "revision": "a2334ff9bc68c321bce67af5ebb4ec33",
+    "url": "/cahoots/static/js/2.80f76055.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7799288b73964b6e76",
+    "url": "/cahoots/static/js/main.d40d2239.chunk.js"
+  },
+  {
+    "revision": "fbc68c525a1a0b3e0364",
+    "url": "/cahoots/static/js/runtime-main.0c876a5a.js"
+  }
+]);
